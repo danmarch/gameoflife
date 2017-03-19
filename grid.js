0f44f8184420b75410b4f99d0f5697f23b7d4782
Joseph Simonian <j.simonian10@gmail.com>
2017-03-19 16:33:20 -0700

    Start work

--- grid.js
+++ grid.js
@@ -1,61 +1,38 @@
-function gridData() {
-	var data = new Array();
-	var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
-	var ypos = 1;
-	var width = 50;
-	var height = 50;
-	var click = 0;
+var $container = $("<div></div>").css("float", "left");
+var boxids = [];
+var grid = [];
 
-	// iterate for rows
-	for (var row = 0; row < 10; row++) {
-		data.push( new Array() );
-
-		// iterate for cells/columns inside rows
-		for (var column = 0; column < 10; column++) {
-			data[row].push({
-				x: xpos,
-				y: ypos,
-				width: width,
-				height: height,
-				click: click
-			})
-			// increment the x position. I.e. move it over by 50 (width variable)
-			xpos += width;
-		}
-		// reset the x position after a row is complete
-		xpos = 1;
-		// increment the y position for the next row. Move it down 50 (height variable)
-		ypos += height;
+function addcontainer(n, m) {
+	for (var i = 0; i < n; i++) {
+		grid[i] = [];
+	  for (var j = 0; j < m; j++) {
+			grid[i][j] = 0;
+			var boxij = $("<div></div>");
+			var boxid = i+","+j;
+			boxids.push(boxid);
+			$(boxij).attr('id', boxid);
+	    boxij.addClass("box").appendTo($container);
+	  }
+	  $("<div></div>").css("clear", "both").appendTo($container);
 	}
-	return data;
+
+	$container.appendTo($(".container"));
+
 }
 
-var gridData = gridData();
-// I like to log the data to the console for quick debugging
-console.log(gridData);
+// Add simple box on setup
+$(function() {addcontainer(20, 20)});
 
-var grid = d3.select("#grid")
-	.append("svg")
-	.attr("width","510px")
-	.attr("height","510px");
-
-var row = grid.selectAll(".row")
-	.data(gridData)
-	.enter().append("g")
-	.attr("class", "row");
-
-var column = row.selectAll(".square")
-	.data(function(d) { return d; })
-	.enter().append("rect")
-	.attr("class","square")
-	.attr("x", function(d) { return d.x; })
-	.attr("y", function(d) { return d.y; })
-	.attr("width", function(d) { return d.width; })
-	.attr("height", function(d) { return d.height; })
-	.style("fill", "#fff")
-	.style("stroke", "#222")
-	.on('click', function(d) {
-       d.click = 1 - d.click;
-			 if ((d.click) == 0 ) { d3.select(this).style("fill","#fff"); }
-		   if ((d.click) == 1 ) { d3.select(this).style("fill","#2C93E8"); }
-    });
+$(function() {
+	boxids.forEach( function(id) {
+		document.getElementById(id).onclick = function() {
+			var [i, j] = id.split(",").map(Number);
+			var on = ++grid[i][j];
+			if (on % 2 == 1) {
+				document.getElementById(id).style.backgroundColor = "#222";
+			} else {
+				document.getElementById(id).style.backgroundColor = "#fff";
+			}
+		}
+	})
+});
